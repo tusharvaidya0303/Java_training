3a49989c264bf2ccd1c94cdd106cf6db4a936fe4
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-04-16 12:42:45 +0530

    heelo

---
+++ loops/heelo.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class heelo {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Hello World!");
+            i++;
+        }
+    }
+}
