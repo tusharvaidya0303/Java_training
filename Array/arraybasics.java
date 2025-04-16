5444fd492d069947d58e456eca421e2a8b4d23cf
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-04-16 15:58:40 +0530

    array

---
+++ Array/arraybasics.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class arraybasics {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,6};
+
+        for(int i=0;i<=arr.length-1;i++){
+            System.out.print(arr[i]);
+        }
+
+    }
+}
