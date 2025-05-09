2ed162ab1f7faa66bc460e712d55f42b51044c0d
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-05-09 22:46:45 +0530

    Java

--- Basics/hello.java
+++ Basics/hello.java
@@ -2,7 +2,6 @@ package Basics;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println(22);
+        System.out.println("Hello World");
     }
 }
