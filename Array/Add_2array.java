aca7be4090a98face8144e9c37cf3ee94543c9c5
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-05-20 12:31:46 +0530

    Java

--- Array/Add_2array.java
+++ Array/Add_2array.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class Add_2array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr1={1,3,5,7,9};
-        int[] arr2={2,4,6,8,10};
+        int[] arr1={1,3,5,7};
+        int[] arr2={2,4,6,8};
         int[] sum=new int [arr1.length];
         for(int i=0;i<arr1.length;i++){
             sum[i]=arr1[i]+arr2[i];
