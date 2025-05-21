4620ca423fe914792c56e821aab7481338c94ce5
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-05-21 10:46:39 +0530

    Java

---
+++ Inheritance/Multiple.java
@@ -0,0 +1,17 @@
+//package Inheritance;
+//class A {
+//    void sum() {
+//        System.out.println("This is A");
+//    }
+//}
+//    class B{
+//
+//        void sum(){
+//            System.out.println("This is B");
+//        }
+//}
+//public class Multiple extends A,B {
+//        Multiple obj = new Multiple();
+//        obj.
+//}
+//Multiple Inheritance IS not supported in Java instead Interface is used.
