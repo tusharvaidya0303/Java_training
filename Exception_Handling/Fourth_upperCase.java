cbbd35af40eaa0160221038f23c96208e124826e
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-05-25 20:44:07 +0530

    Java

---
+++ Exception_Handling/Fourth_upperCase.java
@@ -0,0 +1,11 @@
+package Exception_Handling;
+import java.util.*;
+public class Fourth_upperCase {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String str = sc.nextLine();
+        System.out.println(str);
+        sc.close();
+    }
+}
