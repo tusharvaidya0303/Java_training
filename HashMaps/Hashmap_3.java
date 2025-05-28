dd0e988d37a8513f40f9013c1b472923ed4b7cb0
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-05-28 14:06:10 +0530

    Java

---
+++ HashMaps/Hashmap_3.java
@@ -0,0 +1,15 @@
+package HashMaps;
+
+import java.util.HashMap;
+
+public class Hashmap_3 {
+    public static void main(String[] args) {
+        String str="java is programming and programming is java";
+        String[] newStr=str.split(" ");
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String i : newStr){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+        System.out.println(map);
+    }
+}
---
+++ HashMaps/Hashmap_4.java
@@ -0,0 +1,20 @@
+package HashMaps;
+
+import java.util.HashMap;
+
+public class Hashmap_4 {
+    public static void main(String[] args) {
+        String str="aabbcddef";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : str.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        System.out.println("Characters with frequency 1 : ");
+        for (char ch : map.keySet()) {
+            if (map.get(ch) == 1)
+            {
+                System.out.print(ch);
+            }
+        }
+    }
+}
