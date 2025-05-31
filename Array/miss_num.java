ef289ceaa16273349308fe003831672e879f5b6e
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-05-31 18:50:49 +0530

    Java

--- Array/miss_num.java
+++
@@ -1,8 +0,0 @@
-package Array;
-
-public class miss_num {
-    public static void main(String[] args) {
-        int[] arr ={0,1,2,4,5};
-
-    }
-}
---
+++ Array/reverse.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class reverse {
+    public static void main(String[] args) {
+        int[] arr ={0,1,2,4,5};
+        for (int i=arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
