6ef46b55346395595c7fbcd4806d1eb497b77a17
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2025-09-26 17:44:30 +0530

    .

--- Exception_Handling/Third_str.java
+++ Exception_Handling/Third_str.java
@@ -1,13 +1,13 @@
-package Exception_Handling;
+// package Exception_Handling;
 
-public class Third_str {
-    public static void main(String[] args) {
-        String str = null;
-        try {
-            System.out.println("Length of string : "+str.length());
-        }catch (Exception e){
-            System.out.println("String is Null"+e);
-        }
+// public class Third_str {
+//     public static void main(String[] args) {
+//         String str = null;
+//         try {
+//             System.out.println("Length of string : "+str.length());
+//         }catch (Exception e){
+//             System.out.println("String is Null"+e);
+//         }
 
-    }
-}
+//     }
+// }
