d77c33f562e2321511f106016b1f9bb471197041
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-11 21:01:10 +0530

    sum

--- 03_loops/sum.java
+++ 03_loops/sum.java
@@ -6,16 +6,24 @@ public class sum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int evenSum = 0;
+        int oddSum = 0;
+        int choice = 0;
+
+
         do{
             System.out.println("Enter the number : ");
             int number = sc.nextInt();
 
             if (number % 2 == 0) {
-                int evenSum += number;
+                evenSum += number;
             }else{
-                int oddSum += number;
+                oddSum += number;
             }
-            
-        }
+            System.out.println("Do you want to continue? Press1 for yes or 0 for no");
+            choice = sc.nextInt();
+        }while(choice == 1);
+        System.out.println("Sum of Even numbers : "+evenSum);
+        System.out.println("Sum of Odd numbers : "+oddSum);
     }
 }
