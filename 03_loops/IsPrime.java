16816a20f2d844f9bdfd95bb72ea08812187c881
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-11 20:31:18 +0530

    IsPrime

--- 03_loops/IsPrime.java
+++ 03_loops/IsPrime.java
@@ -6,17 +6,18 @@ public class IsPrime {
         System.out.print("Enter the number : ");
         int n = sc.nextInt();
 
-        boolean IsPrime = true;
+        boolean isPrime = true;
 
         if(n==2){
             System.out.println(n+" "+ "is a prime number");
         }else{
                 for(int i = 2 ; i<= Math.sqrt(n); i++){
-                    if(n%i==0){          // n is a multiple of i (i is not equla to 1 or n)
-                    IsPrime = false;
+                    if(n%i==0){                                  // n is a multiple of i (i is not equla to 1 or n)
+                    isPrime = false;
+                    break;                                      //stop checking once divisor is found
             }
         }
-        if (IsPrime == true) {
+        if (isPrime == true) {
             System.out.println(n+" "+ "is a prime number");
         }else{
             System.out.println(n+" "+"is not a prime number");
