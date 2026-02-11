140852bf43d70a0cf6979522ab5f15cd41ec6426
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-11 20:25:53 +0530

    IsPrime

--- 03_loops/IsPrime.java
+++ 03_loops/IsPrime.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class IsPrime {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enter the number : ");
+        System.out.print("Enter the number : ");
         int n = sc.nextInt();
 
         boolean IsPrime = true;
@@ -12,7 +12,7 @@ public class IsPrime {
             System.out.println(n+" "+ "is a prime number");
         }else{
                 for(int i = 2 ; i<= Math.sqrt(n); i++){
-                    if(n%10==0){          // n is a multiple of i (i is not equla to 1 or n)
+                    if(n%i==0){          // n is a multiple of i (i is not equla to 1 or n)
                     IsPrime = false;
             }
         }
