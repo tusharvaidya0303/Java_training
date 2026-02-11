e461d50618f53fb4f652a4758a3c87f3ec489dd7
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-11 20:48:47 +0530

    sum

---
+++ 03_loops/sum.java
@@ -0,0 +1,16 @@
+//Write a program that reads a set of integers, and then prints the sum of the even and odd integers.
+
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("Enter the number : ");
+            int number = sc.nextInt();
+
+            
+        }
+    }
+}
