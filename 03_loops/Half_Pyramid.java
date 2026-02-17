3f5604eb8216aa2be175b9f5a225d597a8a194b5
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-17 19:00:17 +0530

    pattern

--- 03_loops/Half_Pyramid.java
+++ 03_loops/Half_Pyramid.java
@@ -18,6 +18,28 @@ public class Half_Pyramid {
             System.out.println();
         }
 
+        System.out.println("Number Half Pyramid");
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        char ch = 'A';
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+
+        
+
+
 
 
     }
