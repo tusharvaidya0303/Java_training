5ba0af553fe3b31120f9542e6523a7663ed4e618
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-18 18:43:24 +0530

    Is prime Functions

--- 04_Function_Methods/primeNum.java
+++ 04_Function_Methods/primeNum.java
@@ -1,23 +1,22 @@
 
 import java.util.*;
 public class primeNum {
-    public static void checkPrimeNum(int a,int count){
-        for(int i=1;i<=a;i++){
-            if(a%i==0) {
-                count++;
+    public static boolean isPrime(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0) {
+                return false;
             }
         }
-        if(count==2){
-                System.out.print(a+" is a Prime number");
-        }else
-                System.out.println(a+" is not a prime number");
+        return true;
         }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count=0;
         System.out.print("Enter the no. : ");
-        int a = sc.nextInt();
-        checkPrimeNum(a,count);
+        int n = sc.nextInt();
+        System.out.println(isPrime(n));
         sc.close();
     }
 }
