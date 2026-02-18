f13acea974574b19ebc2ac18e1954b779b1425a5
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-18 18:57:12 +0530

    Is prime Functions

--- 04_Function_Methods/primeNum.java
+++ 04_Function_Methods/primeNum.java
@@ -12,11 +12,24 @@ public class primeNum {
         }
         return true;
         }
+
+    public static void primesInRange(int n){
+
+        for(int i = 2; i <= n; i++){
+            if(isPrime(i)){ //true
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. : ");
         int n = sc.nextInt();
         System.out.println(isPrime(n));
+
+        primesInRange(20); // all primes in range from 2 to 20
+
         sc.close();
     }
 }
