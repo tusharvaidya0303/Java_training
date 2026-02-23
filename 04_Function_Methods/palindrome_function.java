a1997bbd853a77e548239d85319ea2bd8dd9270c
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-23 20:49:26 +0530

    palindrome function

--- 04_Function_Methods/palindrome_function.java
+++ 04_Function_Methods/palindrome_function.java
@@ -1,3 +1,20 @@
 public class palindrome_function {
-    
+    public static boolean isPalindrome(int n){
+        int number = n;
+        int reverse = 0;
+        while(number > 0){
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        if(reverse == n){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(123));
+    }
 }
