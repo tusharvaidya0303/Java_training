6ac383d6ac8accddd88e99da1da8b7dc129994ff
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-24 12:50:15 +0530

    bin

--- Sharing/addition.java
+++
@@ -1,7 +0,0 @@
-package Sharing;
-
-public class addition {
-    void display1(int i,int j){
-        System.out.println("Sum : "+(i+j));
-    }
-}
--- String/String1.java
+++
@@ -1,9 +0,0 @@
-package String;
-
-public class String1 {
-    public static void main(String[] args) {
-        String str="hello";
-        System.out.println(str.charAt(4));
-        System.out.println(str.indexOf("o"));
-    }
-}
