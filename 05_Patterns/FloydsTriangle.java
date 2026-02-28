400b5115a30c18101d2b3b0fd8b81024da40551f
Tushar Vaidya <tusharvaidya0303@gmail.com>
2026-02-28 20:38:32 +0530

    Floyd triangle

---
+++ 05_Patterns/FloydsTriangle.java
@@ -0,0 +1,16 @@
+
+public class FloydsTriangle {
+    public static void printFloyds(int n){
+        int counter = 1;
+        for(int i = 1; i<=n;i++){
+            for (int j = 1;j<=i;j++){
+                System.out.print(" "+counter);
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        printFloyds(5);
+    }
+}
