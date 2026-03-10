b61886e05b7abdf7b38d0c9c7f0c2b00608b3401
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-03-10 10:12:10 +0530

    Sliding Window

---
+++ Arrays/Sliding Window/Sum.java
@@ -0,0 +1,16 @@
+package Sliding Window;
+
+public class Sum {
+    public static void main(String[] args) {
+        int [] arr ={1,2,3,4,5,6,7,10};
+        int k = 4;
+        int sum = 0;
+        for(int i = 0; i<k;i++){
+            sum += arr[i];
+        }
+        int max = sum;
+        for(int i=k;i<arr.length;i++){
+            
+        }
+    }
+}
--- Arrays/Two Pointer/RemoveDuplicate.java
+++ Arrays/Two Pointer/RemoveDuplicate.java
@@ -1,9 +1,9 @@
 public class RemoveDuplicate {
     public static void main(String[] args) {
         int arr[]={2,3,4,5,5,6,7,7,9};
-        int j =0;
+        int j = 0;
 
-        for(int i = 0;i<=arr.length-1;i++){
+        for(int i = 1;i < arr.length;i++){
             if (arr[i]!=arr[j]) {
                 j++;
                 arr[j] = arr[i];
