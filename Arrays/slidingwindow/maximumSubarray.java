97b06bdf104600284ddbb5b741b75ccf653c5f62
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-03-11 13:31:56 +0530

    SlidingWindow

---
+++ Arrays/slidingwindow/maximumSubarray.java
@@ -0,0 +1,23 @@
+package slidingwindow;
+
+public class maximumSubarray {
+    public static void main(String[] args) {
+
+        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+
+        int Global_max = arr[0];
+        int Current_max = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+
+            if(Current_max < 0){
+                Current_max = 0;
+            }
+
+            Current_max += arr[i];
+            Global_max = Math.max(Current_max, Global_max);
+        }
+
+        System.out.println("Maximum Subarray Sum: " + Global_max);
+    }
+}
