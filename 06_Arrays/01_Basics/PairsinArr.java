94f2e5e4f7c115f752f402e9af31aa46fac01016
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-03-12 21:09:25 +0530

    pairs in an array

---
+++ 06_Arrays/01_Basics/PairsinArr.java
@@ -0,0 +1,18 @@
+public class PairsinArr {
+    public static void printPairs(int arr[]){
+        int total=0;
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            for (int j=i+1; j < arr.length; j++) {
+                System.out.print( "(" + curr + "," + arr[j] + ") " );
+                total++;
+            }
+            System.out.println();
+        }
+        System.out.println("Total Pairs : "+ total);
+    }
+    public static void main(String[] args) {
+        int arr[]={2,4,6,8,10};
+        printPairs(arr);
+    }
+}
