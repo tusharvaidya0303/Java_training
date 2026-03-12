a7f483277e18471a5ebd55709521a83c173454a5
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-03-12 10:22:58 +0530

    Arrays Basics

--- 05_Arrays/slidingwindow/maximumSubarray.java
+++ 05_Arrays/slidingwindow/maximumSubarray.java
@@ -1,8 +1,7 @@
-package slidingwindow;
-
 public class maximumSubarray {
     public static void main(String[] args) {
 
+        
         int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
 
         int Global_max = arr[0];
