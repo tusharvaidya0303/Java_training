b00f78a796908b24ff45fee24ad4d5ccc98aaa7a
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-03-28 17:03:03 +0530

    Max subarray sum

--- 06_Arrays/slidingwindow/MaxSum.java
+++ 06_Arrays/slidingwindow/MaxSum.java
@@ -1,28 +1,21 @@
 public class MaxSum {
+    public static int returnSum(int[] arr, int k) {
+        int maxSum = 0;
+        int cursum = 0;
+        for(int i = 0; i < k; i++){
+            cursum += arr[i];
+        }
+        maxSum = cursum;
+        for(int i = k; i < arr.length; i++){
+            cursum += arr[i] - arr[i-k];
+            maxSum = Math.max(maxSum, cursum);
+        }
+        return maxSum;
+    }
     public static void main(String[] args) {
 
-        int[] arr = {1,2,3,4,5,6,7,10};
+        int[] arr = {1,12,-5,-6,50,3};
         int k = 4;
-
-        int sum = 0;
-
-        // first window
-        for(int i = 0; i < k; i++){
-            sum += arr[i];
-        }
-
-        int max = sum;
-
-        // sliding window
-        for(int i = k; i < arr.length; i++){
-            sum += arr[i];       // add next element
-            sum -= arr[i-k];     // remove previous element
-
-            if(sum > max){
-                max = sum;
-            }
-        }
-
-        System.out.println("Max sum : " + max);
+        System.out.println(returnSum(arr, k));
     }
 }
