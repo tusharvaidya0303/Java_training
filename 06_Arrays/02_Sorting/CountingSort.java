e51e7f72bd8611b29487a97e5c1ba6dc393e68a5
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-04 17:47:13 +0530

    Counting Sort

---
+++ 06_Arrays/02_Sorting/CountingSort.java
@@ -0,0 +1,32 @@
+public class CountingSort {
+    public static void Sorted(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i]>max) {
+                max = Math.max(max, nums[i]);
+            }
+        }
+        int[] freq = new int[max+1];
+        for (int i = 0; i < nums.length; i++) {
+            freq[nums[i]]++; 
+        }
+        int j=0;
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i]>0) {
+                nums[j++] = i;
+                freq[i]--; 
+            }
+        }
+    }
+    public static void print(int[] nums){
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+",");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int nums[]={5,4,1,3,2};
+        Sorted(nums);
+        print(nums);
+    }   
+}
