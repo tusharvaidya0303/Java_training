e595277bc59a6f3a5570390a79109be6c3ae11d9
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-06 21:04:55 +0530

    Staircase Searching

---
+++ 07_2Darrays/StaircaseSearch.java
@@ -0,0 +1,24 @@
+public class StaircaseSearch {
+    public static boolean staircaseSearch(int matrix[][], int key) {
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == key) {
+                System.out.println("Key found at (" + row + ", " + col + ")");
+                return true;
+            } else if (matrix[row][col] > key) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("Not found");
+        return false;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 }, 
+                          { 4, 5, 6 }, 
+                          { 7, 8, 9 } };
+        int key = 5;
+        staircaseSearch(matrix, key);
+    }
+}
