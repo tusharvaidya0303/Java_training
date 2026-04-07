11c0763b4f2be85898a86e43bac5724049306c0b
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-04-07 18:44:37 +0530

    String Basics

---
+++ 08_Strings/basics.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // String str = "Hello World";
+        // String str2  = new String("Hello World");
+        // System.out.println(str .equals(str2));        //str == str2 -> false because str and str2 are not same object.
+
+        //input from user
+    //     System.out.println("Enter a string : ");
+    //     String str3 = sc.nextLine();             //sc.next(); only prints one word
+    //     System.out.println(str3);
+    // }
+
+    //concatenate
+    // String firstName = "Tony";
+    // String lastName = "Stark";
+    // String fullName = firstName + " " + lastName;
+    // System.out.println(fullName);
+
+    //length
+    // String str = "Hello World";
+    // System.out.println(str.length());
+
+    //charAt
+    // String str = "Hello World";
+    // System.out.println(str.charAt(0));             //index starts from 0 prints character from given index
+
+    //indexOf
+    // String str = "Hello World";
+    // System.out.println(str.indexOf("o"));         //prints index of given character in string
+
+    }
+}
