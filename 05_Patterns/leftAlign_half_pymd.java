479ee4326907033809bd128fb2fcd3b3d72d68cf
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-07-03 13:09:37 +0530

    pattern

--- 05_Patterns/leftAlign_half_pymd.java
+++ 05_Patterns/leftAlign_half_pymd.java
@@ -53,6 +53,7 @@ public class leftAlign_half_pymd {
         for (int i = 1; i<=n ; i++){
             for(int j = 1;j<=i;j++){
                 System.out.print((char)(c+(n-i)+(j-1)));
+                
             }
             System.out.println();
         }
