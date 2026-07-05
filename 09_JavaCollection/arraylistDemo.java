081c6cb1ef064277ea04b8221858340ef099c9bd
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-07-05 19:57:41 +0530

    java collection arraylist

--- 09_JavaCollection/arraylistDemo.java
+++ 09_JavaCollection/arraylistDemo.java
@@ -13,9 +13,9 @@ public class arraylistDemo {
 
         //using addall
         ArrayList<Integer> arr2 = new ArrayList<>();
-        arr.add(100);
-        arr.add(200);
-        arr.add(30);
+        arr2.add(100);
+        arr2.add(200);
+        arr2.add(30);
 
         arr.addAll(arr2);
         System.out.println(arr);
@@ -31,11 +31,60 @@ public class arraylistDemo {
         System.out.println("list of arr2 : "+arr2);
         arr2.clear();
         System.out.println("size of arr2 :"+arr2.size());
+
+        //Iterator
+
+        Iterator<Integer> iterator = arr.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("Elements : "+ iterator.next());
+        }
         
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        //Get
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        //Set
+        System.out.println("Before : "+list);
+        list.set(2, 300);
+        System.out.println("After : "+list);
 
+        //Toarray
+        Object array[] = list.toArray();
+        for (Object object : array) {
+            System.out.print(object+" ");
+        }
 
+        //Contains
+        System.out.println(list.contains(300));
 
+        //Sort
+        list.add(6);
+        list.add(9);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
 
+        //Clone
+        //deep copy
+        // ArrayList<Integer> newList = new ArrayList<>(list);
+        //Shallow copy
+        // System.out.println(list);
+        // ArrayList <Integer> newlist = (ArrayList<Integer>)list.clone(); 
+        // System.out.println("NewList : "+newlist);
+
+        ArrayList <Integer> marks = new ArrayList<>();
+        //Max Capacity
+        marks.ensureCapacity(100);  
+        //isempty
+        System.out.println(marks.isEmpty());
+        //indexof tells index
+
+        System.out.println(list.indexOf(6));
     }
     
 }
