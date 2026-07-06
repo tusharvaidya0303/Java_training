7bbce131d4bfa9c05ba3427ed2eb325e4fc289f7
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-07-06 18:22:59 +0530

    Simple maths problems

--- 05.5_Maths/DigitCount.java
+++ 05.5_Maths/DigitCount.java
@@ -1,9 +1,21 @@
 public class DigitCount {
-        public static int count(int num) {
+
+    //Optimized
+    public static int count(int num) {
         int cnt = 0;
             cnt = (int)(Math.log10(num)+1);
         return cnt;
     }
+
+    // public static int count1(int num) {
+    //     int cnt = 0;
+    //     while (num != 0) {
+    //         num = num / 10;
+    //         cnt++;
+    //     }
+    //     return cnt;
+    // }
+
     public static void main(String[] args) {
         int num = 12345;
         System.out.println("No. of digits present in " + num + " is " + count(num));
