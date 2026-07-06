756440891169703cc16bb353a8be976d16d7f10c
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-07-06 17:07:34 +0530

    java collection HashSets

---
+++ 09_JavaCollection/hashSetsDemo.java
@@ -0,0 +1,29 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+public class hashSetsDemo {
+    public static void main(String[] args) {
+
+        //1. HashSet
+        Set<Integer> set = new HashSet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        System.out.println(set);       // No Guarantee of order randome order
+
+        //2. LinkedHashSet
+        Set<Integer> set2 = new LinkedHashSet<>();
+        set2.add(10);
+        set2.add(20);
+        set2.add(30);
+        System.out.println(set2);      //Guarantee of order same order as added
+
+        //3. TreeSet
+        Set<Integer> set3 = new TreeSet<>();
+        set3.add(10);
+        set3.add(20);
+        set3.add(30);
+        System.out.println(set3);      //Guarantee of order and sorted use binary search
+    }
+}
