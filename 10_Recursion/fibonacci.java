820d93b1eb3970d992af16a0deaace9f1c9fc1e5
tusharvaidya0303 <tusharvaidya0303@gmail.com>
2026-07-07 21:45:26 +0530

    Recursion Basics

---
+++ 10_Recursion/fibonacci.java
@@ -0,0 +1,15 @@
+public class fibonacci {
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(fib(n));
+    }
+}
